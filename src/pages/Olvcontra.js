dd9ed388d7a62669f9c9119917792f1c2daa81c3
alejandrosanfer <alejandrosanfer42@gmail.com>
2023-10-06 00:49:43 -0500

    Se agregó la funcionalidad de olvido de contraseña

---
+++ src/pages/Olvcontra.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Olvicontra from '../components/Olvido contraseña/Olvicontra'
+ function Olvcontra() {
+  return (
+    <div>
+      <Olvicontra/>
+    </div>
+  )
+}
+
+export default Olvcontra
