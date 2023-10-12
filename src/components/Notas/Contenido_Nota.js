2b7b1dfe30eb8a89ab1f2f86eb9852a29e3393a6
Luis_Gomez <luisalbertogomezbroca@gmail.com>
2023-10-11 20:20:07 -0500

    Se implemento endpoint put

--- src/components/Notas/Contenido_Nota.js
+++ src/components/Notas/Contenido_Nota.js
@@ -10,38 +10,25 @@ function ContenidoNota({ notaUtilizar }) {
     Fecha_Creacion: "2023-10-10"
   });
 
-  //UPDATE
-
-  // const actualizarNota = async () => {
-  //   try {
-  //     const response = await fetch(`http://localhost:4000/Notas/${notaUtilizar.Id_Nota}`, {
-  //       method: 'PUT',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify(nota),
-  //     });
-
-  //     if (response.ok) {
-  //       const data = await response.json();
-  //       setNota(data);
-  //     } else {
-  //       console.error('Error al realizar la solicitud.');
-  //     }
-  //   } catch (error) {
-  //     console.error('Error de red:', error);
-  //   }
-
-  // }
-
-
+  const [notaSeleccionada, setNotaSeleccionada] = useState({
+    Titulo: "",
+    Contenido: "",
+    FK_Usuario: "1",
+    Fecha_Creacion: "2023-10-10"
+  });
 
 
   useEffect(() => {
     if (notaUtilizar) {
-      setNota({
+      setNotaSeleccionada({
         Titulo: notaUtilizar.Titulo,
         Contenido: notaUtilizar.Contenido,
+      });
+    }
+    else {
+      setNota({
+        Titulo: "",
+        Contenido: "",
       });
     }
   }, [notaUtilizar]);
@@ -68,19 +55,60 @@ function ContenidoNota({ notaUtilizar }) {
     }
   };
 
+  const actualizarNota = async () => {
+    try {
+      const response = await fetch(`http://localhost:4000/Notas/${notaUtilizar.Id_Nota}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          Titulo: notaSeleccionada.Titulo,
+        }),
+      });
+      if (response.ok) {
+        console.log('Nota actualizada correctamente.');
+        console.log(notaSeleccionada);
+        window.location.reload();
+      } else {
+        console.error('Error al actualizar la nota.');
+      }
+    } catch (error) {
+      console.error('Error de red:', error);
+    }
+  };
+
+  function consola(e) {
+    console.log(notaSeleccionada)
+  }
 
 
 
   return (
     <div className=' h-screen overflow-auto w-full'>
-      <input className='font-PassionOne text-7xl mt-5 mb-3 outline-none w-full' placeholder='Ingresa un titulo' value={nota.Titulo} onChange={(e) => setNota({ ...nota, Titulo: e.target.value })} />
+      {(notaUtilizar) ? <input className='font-PassionOne text-7xl mt-5 mb-3 outline-none w-full' placeholder='Ingresa un titulo' value={notaSeleccionada.Titulo} onChange={(e) => setNotaSeleccionada({ ...notaSeleccionada, Titulo: e.target.value })} />
+        : <input className='font-PassionOne text-7xl mt-5 mb-3 outline-none w-full' placeholder='Ingresa un titulo' value={nota.Titulo} onChange={(e) => setNota({ ...nota, Titulo: e.target.value })} />}
       <div className='mx-5 mt-5 flex flex-col justify-center items-center'>
-        <textarea
-          placeholder='Escribe el contenido de la nota'
-          className='font-mono w-full outline-none resize-none h-96'
-          value={nota.Contenido} onChange={(e) => setNota({ ...nota, Contenido: e.target.value })}
-        ></textarea>
-        <button className='font-PassionOne text-3xl mx-3 mt-5 mb-3 outline-none hover:bg-blue-400 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline transition-colors duration-300 ease-in-out w-40 h-14 tareasenproceso' type='button' name='button' onClick={enviarNota}>Guardar</button>
+        {
+          (notaUtilizar) ? <textarea
+            placeholder='Escribe el contenido de la nota'
+            className='font-mono w-full outline-none resize-none h-96'
+            value={notaSeleccionada.Contenido} onChange={(e) => setNota({ ...notaSeleccionada, Contenido: e.target.value })}
+          ></textarea>
+            :
+            <textarea
+              placeholder='Escribe el contenido de la nota'
+              className='font-mono w-full outline-none resize-none h-96'
+              value={nota.Contenido} onChange={(e) => setNota({ ...nota, Contenido: e.target.value })}
+            ></textarea>
+        }
+        {
+          (notaUtilizar) ? <button className='font-PassionOne text-3xl mx-3 mt-5 mb-3 outline-none hover:bg-blue-400 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline transition-colors duration-300 ease-in-out w-40 h-14 tareasenproceso' type='button' name='button' onClick={actualizarNota}>actualizar</button>
+            : <button className='font-PassionOne text-3xl mx-3 mt-5 mb-3 outline-none hover:bg-blue-400 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline transition-colors duration-300 ease-in-out w-40 h-14 tareasenproceso' type='button' name='button' onClick={enviarNota}>Guardar</button>
+
+        }
+        <button className='font-PassionOne text-3xl mx-3 mt-5 mb-3 outline-none hover:bg-blue-400 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline transition-colors duration-300 ease-in-out w-40 h-14 tareasenproceso' type='button' name='button' onClick={consola}>Guardar</button>
+
       </div>
     </div>
   )
