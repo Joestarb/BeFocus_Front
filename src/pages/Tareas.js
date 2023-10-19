e89691c6613c77edf23d5573563e77f5b9277416
Arbey DzIb <120668243+Joestarb@users.noreply.github.com>
2023-10-19 18:17:30 -0500

    fix tareas page

--- src/pages/Tareas.js
+++ src/pages/Tareas.js
@@ -6,8 +6,12 @@ import TareasP from '../components/Tareas/TareasP'
 function Tareas() {
     return (
         <div className=' flex '>
+            <div className='fixed'>
             <Sidebar />
+            </div>
+            <div className=' overflow-auto mx-10'>
             <TareasP />
+            </div>
         </div>
     )
 }
