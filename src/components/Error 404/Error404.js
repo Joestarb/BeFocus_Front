da71a753d4e1be17de0f5ff377afee4ed0280416
Luis_Gomez <luisalbertogomezbroca@gmail.com>
2023-11-27 18:06:38 -0500

    Se reparo olv contraseña

--- src/components/Error 404/Error404.js
+++ src/components/Error 404/Error404.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Footer from "../../components/Index/Footer";
 
 function Error404() {
   return (
-    <div>Error404</div>
-  )
+    <>
+      <div className='grid place-content-center h-screen w-full'>
+        <h2 className='md:text-5xl text-3xl font-bold text-center italic text-zinc-800 mb-3'>Oops! Página no encontrada</h2>
+        <img src='https://cdn.dribbble.com/users/550484/screenshots/2128340/media/ab10d36499f3f14a8749486c80d3cafe.gif' alt='Error 404' className='  w-3/4 mx-auto ' />
+        
+        <div className="flex flex-col items-center">
+          <p className='text-center text-zinc-800 font-medium italic text-lg md:mx-auto mx-5'>Lo sentimos, la página que estás buscando no existe.</p>
+          <p className='text-center text-zinc-800 font-medium italic text-lg md:mx-auto mx-5'>Pero aquí tienes algunas cosas que podrías hacer:</p>
+
+          <div className='flex flex-col mt-5'>
+            <p className='font-medium text-lg mx-5 text-center'>
+              ¿Quieres volver al inicio? <Link to='/' className='font-bold text-lg text-[#7895B2] text-center'>Ir a la página principal</Link>
+            </p>
+          </div>
+        </div>
+
+      </div>
+      <Footer />
+    </>
+  );
 }
 
-export default Error404
+export default Error404;
--- src/components/Olvido contraseña/Olvicontra.js
+++ src/components/Olvido contraseña/Olvicontra.js
@@ -11,7 +11,7 @@ function Olvicontra() {
   const [confirmPassword, setConfirmPassword] = useState(''); // New state for the confirmation password
   const [isPasswordChanged, setIsPasswordChanged] = useState(false);
   const [isVerificationModalOpen, setIsVerificationModalOpen] = useState(false);
-  const [casillas, setCasillas] = useState(['', '', '', '', '', '', '', '']);
+  const [casillas, setCasillas] = useState(['', '', '', '', '', '']);
   const [isChangePasswordModalOpen, setIsChangePasswordModalOpen] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
 
@@ -20,9 +20,23 @@ function Olvicontra() {
     setIsChangePasswordModalOpen(true);
   };
 
+  const cerrarModalVerificacion = () => {
+    setIsVerificationModalOpen(false);
+    setCasillas(['', '', '', '', '', ''])
+  }
+
+  //Solicitar cambio de contraseña
   const handleForgotPassword = async (e) => {
     e.preventDefault();
-    console.log('Enviando correo electrónico...')
+    if (email === '' || email === null || email === undefined || email.trim() === '') {
+      Swal.fire({
+        title: "Ingresa tu correo correctamente",
+        icon: "error",
+        confirmButtonText: "Ok"
+      })
+      return;
+    }
+    console.log('Validando cuenta...')
     try {
       setIsSubmitting(true);
       const response = await fetch('http://localhost:4000/olvido-contrasena', {
@@ -33,11 +47,20 @@ function Olvicontra() {
         body: JSON.stringify({ Correo: email }),
       });
 
+      const data = await response.json();
+      if(data.message === "Usuario no encontrado"){
+        Swal.fire({
+          title: "Usuario no encontrado",
+          icon: "error",
+          confirmButtonText: "Ok"
+        })
+      }
       if (response.ok) {
         setIsVerificationModalOpen(true);
       } else {
         console.error('Error al solicitar recuperación de contraseña');
       }
+
     } catch (error) {
       console.error('Error en la solicitud:', error);
     } finally {
@@ -45,12 +68,22 @@ function Olvicontra() {
     }
   };
 
+  //Verificar el codigo
   const verificarCodigo = async () => {
     console.log(email)
     console.log(casillas)
     const cadenaDeCasillas = casillas.join('');
-
     console.log(cadenaDeCasillas);
+
+    if (casillas.some(valor => valor.trim() === '')) {
+      Swal.fire({
+        title: "Completa todos los campos de verificación",
+        icon: "error",
+        confirmButtonText: "Ok"
+      });
+      return;
+    }
+
     try {
       setIsSubmitting(true);
       const response = await fetch('http://localhost:4000/verificar-codigo', {
@@ -60,7 +93,14 @@ function Olvicontra() {
         },
         body: JSON.stringify({ Correo: email, CodigoVerificacion: cadenaDeCasillas }),
       });
-
+      const data = await response.json();
+      if(data.message === "Código de verificación inválido o expirado"){
+        Swal.fire({
+          title: "Código de verificación inválido o expirado",
+          icon: "error",
+          confirmButtonText: "Ok"
+        })
+      }
       if (response.ok) {
         console.log("Codigo valido")
         Swal.fire({
@@ -82,15 +122,32 @@ function Olvicontra() {
     }
   }
 
+  //Cambiar contraseña
   const handleChangePassword = async (e) => {
     e.preventDefault();
+    if (newPassword === '' || newPassword === null || newPassword === undefined || newPassword.trim() === '') {
+      Swal.fire({
+        title: "Ingresa tu nueva contraseña correctamente",
+        icon: "error",
+        confirmButtonText: "Ok"
+      })
+      return;
+    }
+    if (confirmPassword === '' || confirmPassword === null || confirmPassword === undefined || confirmPassword.trim() === '') {
+      Swal.fire({
+        title: "Ingresa tu nueva contraseña correctamente",
+        icon: "error",
+        confirmButtonText: "Ok"
+      })
+      return;
+    }
     try {
       setIsSubmitting(true);
 
       // Check if passwords match
       if (newPassword !== confirmPassword) {
         Swal.fire({
-          title: 'Contraseñas no coinciden',
+          title: 'Las contraseñas no coinciden',
           icon: 'error',
           confirmButtonText: 'Ok',
         });
@@ -102,9 +159,17 @@ function Olvicontra() {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ Contrasena: confirmPassword, Correo: email}),
+        body: JSON.stringify({ Contrasena: confirmPassword, Correo: email }),
       });
-
+      const data = await response.json();
+      if(data.message === "Algo ha salido mal, intentalo mas tarde"){
+        Swal.fire({
+          title: "Algo ha salido mal, intentalo mas tarde",
+          icon: "error",
+          confirmButtonText: "Ok"
+        })
+      
+      }
       if (response.ok) {
         Swal.fire({
           title: "Contraseña cambiada exitosamente",
@@ -118,9 +183,19 @@ function Olvicontra() {
 
       } else {
         console.error('Error al cambiar la contraseña');
+        Swal.fire({
+          title: "Algo ha salido mal, intentalo mas tarde",
+          icon: "error",
+          confirmButtonText: "Ok"
+        })
       }
     } catch (error) {
       console.error('Error en la solicitud:', error);
+      Swal.fire({
+        title: "Algo ha salido mal, intentalo mas tarde",
+        icon: "error",
+        confirmButtonText: "Ok"
+      })
     } finally {
       setIsSubmitting(false);
     }
@@ -145,26 +220,28 @@ function Olvicontra() {
 
         <Modal
           isOpen={isVerificationModalOpen}
-          onRequestClose={() => setIsVerificationModalOpen(false)}
+          onRequestClose={cerrarModalVerificacion}
           contentLabel="Verificación de Código"
         >
-          <div class="text-center h-full w-full flex flex-col jus align-middle">
+          <div class="text-center h-full w-full flex flex-col ">
+          <button className=' text-3xl font-bold mr-auto p-2' onClick={cerrarModalVerificacion}> x </button>
             <h1 className=' text-2xl font-semibold italic mt-10'>Te hemos enviado un email a tu correo, revisa tu bandeja de entrada</h1>
-            <div className='m-auto'>
-              <h2 class="text-2xl font-semibold">Introduce el código de verificación</h2>
-              <div className="my-10 flex m-auto">
+            <div className='m-auto w-full'>
+              <h2 class="md:text-2xl text-lg font-semibold m-auto w-full">Introduce el código de verificación</h2>
+              <div className="my-10 grid grid-cols-6 gap-4 mx-auto xl:mx-[30%]">
                 {casillas.map((valor, index) => (
                   <div
                     key={index}
                     contentEditable
-                    className="w-10 h-16 border border-gray-300 flex items-center justify-center rounded text-center mx-2 text-xl font-semibold italic"
+                    className="md:w-16 w-10 h-16 mx-auto border border-gray-300 flex items-center justify-center rounded text-center text-2xl font-bold italic bg-[#EADBC8] text-zinc-700 outline-none"
                     onInput={(e) => handleCasillaInput(e, index)}
                   >
                     {valor}
                   </div>
                 ))}
               </div>
-              <button class="mt-4 bg-zinc-700 font-semibold text-lg text-white px-4 py-2 rounded" onClick={verificarCodigo}>Verificar código</button>
+              <button class="mt-4 bg-[#1F4172] hover:bg-[#4477CE] font-semibold text-lg text-white px-4 py-2 rounded" onClick={verificarCodigo}>Verificar código</button>
+              
             </div>
           </div>
         </Modal>
@@ -179,35 +256,37 @@ function Olvicontra() {
               Cambia tu contraseña
             </h1>
             <form className="mt-5 flex flex-col" onSubmit={handleChangePassword}>
-            <label className="font-medium text-xl text-gray-700">
-              Nueva Contraseña:
-            </label>
-            <input
-              type="password"
-              className="w-80 p-2 mb-4 border font-medium text-lg text-zinc-800 bg-gray-200 border-gray-300 rounded-xl outline-none"
-              placeholder="Introduce tu nueva contraseña"
-              value={newPassword}
-              required
-              onChange={(e) => setNewPassword(e.target.value)}
-            />
-            <label className="font-medium text-xl text-gray-700">
-              Confirmar Contraseña:
-            </label>
-            <input
-              type="password"
-              className="w-80 p-2 mb-4 border font-medium text-lg text-zinc-800 bg-gray-200 border-gray-300 rounded-xl outline-none"
-              placeholder="Confirma la nueva contraseña"
-              value={confirmPassword}
-              required
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-            <button
-              className="bg-[#1F4172] font-medium text-lg text-white py-2 px-4 w-70 rounded-xl hover:bg-blue-600 my-5"
-              type="submit"
-            >
-              Cambiar Contraseña
-            </button>
-          </form>
+              <label className="font-medium text-xl text-gray-700">
+                Nueva Contraseña:
+              </label>
+              <input
+                type="password"
+                className="w-80 p-2 mb-4 border font-medium text-lg text-zinc-800 bg-gray-200 border-gray-300 rounded-xl outline-none"
+                placeholder="Introduce tu nueva contraseña"
+                value={newPassword}
+                // required
+                minLength={8}
+                onChange={(e) => setNewPassword(e.target.value)}
+              />
+              <label className="font-medium text-xl text-gray-700">
+                Confirmar Contraseña:
+              </label>
+              <input
+                type="password"
+                className="w-80 p-2 mb-4 border font-medium text-lg text-zinc-800 bg-gray-200 border-gray-300 rounded-xl outline-none"
+                placeholder="Confirma la nueva contraseña"
+                value={confirmPassword}
+                // required
+                minLength={8}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              <button
+                className="bg-[#1F4172] font-medium text-lg text-white py-2 px-4 w-70 rounded-xl hover:bg-[#4477CE] my-5"
+                type="submit"
+              >
+                Cambiar Contraseña
+              </button>
+            </form>
           </div>
         </Modal>
 
@@ -230,7 +309,7 @@ function Olvicontra() {
                 required
                 onChange={(e) => setEmail(e.target.value)}
               />
-              <button className="bg-[#1F4172] font-medium text-lg text-white py-2 px-4 w-70 rounded-xl hover:bg-blue-600 my-5" type='submit'>
+              <button className="bg-[#1F4172] font-medium text-lg text-white py-2 px-4 w-70 rounded-xl hover:bg-[#4477CE] my-5" type='submit'>
                 Enviar Correo de Recuperación
               </button>
             </form>
@@ -241,6 +320,7 @@ function Olvicontra() {
           <Modal isOpen={true} contentLabel="Realizando operación" className="modal">
             <div className="text-center h-screen flex justify-center align-middle items-center">
               <p className="text-gray-700 font-semibold m-auto text-4xl my-auto">Realizando operación...</p>
+              <div className="w-20 h-20 border-4 border-t-4 border-gray-700 rounded-full animate-spin"></div>
             </div>
           </Modal>
         )}
